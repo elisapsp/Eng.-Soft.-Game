f9c348a6ae6c8b8715298683060b4baad1dcaeb3
Alex <alexgfh@gmail.com>
2016-05-01 16:03:35 -0300

    Faz controle de versao salvar posicao do jogador, adiciona layers para simplificar relacao do jogador com objetos. Restauracao do estado colocada no objeto GerenciadorJogo.

---
+++ Assets/scripts/GerenciadorJogo.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GerenciadorJogo : MonoBehaviour {
+
+	public Transform player;
+	public Vector3 posicaoSalva;
+	public static GerenciadorJogo instance; //singleton
+
+	void Awake() {
+		if (instance != null) {
+			Debug.LogError("GerenciadorJogo deve ter uma unica instancia");
+		}
+		instance = this;
+	}
+
+	void Update() {
+		if (Input.GetKeyDown("c")) {
+			player.position = posicaoSalva;
+		}
+	}
+}
--- Assets/scripts/controleVersao.cs
+++ Assets/scripts/controleVersao.cs
@@ -1,30 +1,20 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+	Salvar os estados do jogo:
+	Objeto player.
+	Inimigos vivos.
+	Pacotes da fase.   
+*/
 public class controleVersao : MonoBehaviour {
 
-    //Será o Log do commit.
-    public string Log;
+	//Será o Log do commit.
+	public string Log;
 
-    // Use this for initialization
-    void Start()
-    {
-
-        //O que fazer:
-
-        /*
-         Salvar os estados do jogo:
-         Objeto player.
-         Inimigos vivos.
-         Pacotes da fase.
-         
-         */
-    
-    }
-	
-	// Update is called once per frame
-	void Update () {
-	
+	public void OnTriggerEnter2D(Collider2D collider) {
+		GerenciadorJogo.instance.posicaoSalva = collider.transform.position;
+		print(Log);
 	}
-    
+
 }
