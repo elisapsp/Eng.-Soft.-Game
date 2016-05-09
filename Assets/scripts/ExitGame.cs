6ac0ce65f66e45f43f31084ef13e5b0e33a13bca
André Borba Netto Assis <Andr Borba Netto Assis>
2016-05-09 18:07:56 -0300

    Botão Sair do Jogo implementado e funcionando. Quando houver o menu inicial, mudar para redirecionar ao menu inicial, por enquanto, está saindo do jogo totalmente.

---
+++ Assets/scripts/ExitGame.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ExitGame : MonoBehaviour {
+
+    public void exitGame()
+    {
+        //Na realidade, deverá voltar ao menu inicial do jogo, que ainda não foi feito.
+        Debug.Log("Deverá mudar para voltar ao menu inicial do jogo, que ainda não foi feito.");
+        //Temporário.
+        Application.Quit();
+    }
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
