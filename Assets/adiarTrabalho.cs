3030c8a8db9ff910ae2630854c41c8d527874f19
André Borba Netto Assis <Andr Borba Netto Assis>
2016-05-16 21:18:00 -0300

    Criação do botão Adiar, para que se o jogador quiser, ele pode resetar o tempo para voltar do inicio e tentar ganhar o jogo mais rapidamente.

---
+++ Assets/adiarTrabalho.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class adiarTrabalho : MonoBehaviour {
+
+
+    private GameObject Timer;
+
+    public void AdiarTrabalho()
+    {
+        Timer.GetComponent<Timer>().timer = 0;
+    }
+
+	// Use this for initialization
+	void Start () {
+        Timer = GameObject.Find("Canvas/Timer/Panel/Text");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
