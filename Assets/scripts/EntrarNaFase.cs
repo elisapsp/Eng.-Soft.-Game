a6e8e8091c136613ca529b8b694bab4aad2b52a3
André Borba Netto Assis <Andr Borba Netto Assis>
2016-06-11 03:58:16 -0300

    MenuInicial e MenuControle implementados.

---
+++ Assets/scripts/EntrarNaFase.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class EntrarNaFase : MonoBehaviour {
+
+    public string nextScene;
+
+	// Use this for initialization
+	void Start () {
+        
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+
+    }
+
+    
+
+    public void goToScene()
+    {
+        nextScene = EventSystem.current.currentSelectedGameObject.name;
+        Application.LoadLevel(nextScene);
+        
+    }
+
+    public void quitGame()
+    {
+        Application.Quit();
+    }
+
+}
